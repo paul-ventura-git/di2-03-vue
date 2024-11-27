2e6c58e2b662e2d33fbbf54fe08a175974b6b316
paul-ventura-git <j.paul.ventura@gmail.com>
2024-11-27 07:52:43 -0500

    25

--- src/components/T25eventModifiersMouseButton.vue
+++ src/components/T25eventModifiersMouseButton.vue
@@ -3,9 +3,26 @@
     <h1>Example: Right Mouse Button Event</h1>
 
     <div id="example25">
-      <div v-on:click.right="changeColor" v-bind:style="{ backgroundColor:'hsl('+bgColor+',80%,80%)' }">
+      <div 
+        v-on:click.right="changeColor1" 
+        v-bind:style="{ backgroundColor:'hsl('+bgColor1+',80%,80%)' }"
+      >
         <p>Press right mouse button here.</p>
       </div>
+
+      <div 
+        v-on:click.right.ctrl="changeColor2"
+        v-bind:style="{backgroundColor:'hsl('+bgColor2+',80%,80%)'}"
+      >
+        <p>Press right mouse button here.</p>
+      </div>
+      <div 
+        v-on:click.right.prevent="changeColor3"
+        v-bind:style="{backgroundColor:'hsl('+bgColor3+',80%,80%)'}"
+      >
+        <p>Press right mouse button here.</p>
+      </div>
+
     </div>
   </div>
 </template>
@@ -13,12 +30,20 @@
 export default {
   data() {
     return {
-      bgColor : 0
+      bgColor1 : 0,
+      bgColor2 : 0,
+      bgColor3 : 0
     }
   },
   methods: {
-    changeColor() {
-      this.bgColor+=50;
+    changeColor1() {
+      this.bgColor1+=50;
+    },
+    changeColor2() {
+      this.bgColor2+=50
+    },
+    changeColor3() {
+      this.bgColor3+=50
     }
   }
 }
